d00f5c5f70a8caf24f3da65d535189b90469fa4f
Bárbara Romero <bqromero@gmail.com>
2021-09-20 15:30:46 -0300

    Criando métodos e exibindo dados

--- src/br/com/zup/exercicio2/ContaBancaria.java
+++ src/br/com/zup/exercicio2/ContaBancaria.java
@@ -5,13 +5,31 @@ public class ContaBancaria {
   int numeroDaConta;
   double saldo = 0;
 
-  public ContaBancaria(){
+  public ContaBancaria() {
 
   }
 
-  public ContaBancaria(String nomeDoCorrentista, int numeroDaConta){
+  public ContaBancaria(String nomeDoCorrentista, int numeroDaConta) {
     this.nomeDoCorrentista = nomeDoCorrentista;
     this.numeroDaConta = numeroDaConta;
+  }
+
+  public void mostrarDados(){
+    System.out.println("Nome do Titular: " + nomeDoCorrentista);
+    System.out.println("Número da Conta: " + numeroDaConta);
+    System.out.println("Saldo atual: " + saldo);
+  }
+
+  public void saque(double valor){
+    saldo = saldo - valor;
+  }
+
+  public void deposito(double valor){
+    saldo = saldo + valor;
+  }
+
+  public void extrato(){
+    mostrarDados();
   }
 
 }
