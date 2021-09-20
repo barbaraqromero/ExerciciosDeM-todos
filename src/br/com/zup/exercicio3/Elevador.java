34cde9e94c35e7cd1e2931e42532c8b57b862d5c
Bárbara Romero <bqromero@gmail.com>
2021-09-20 16:22:24 -0300

    Criando os métodos e exibindo os dados

--- src/br/com/zup/exercicio3/Elevador.java
+++ src/br/com/zup/exercicio3/Elevador.java
@@ -6,14 +6,47 @@ public class Elevador {
   int capacidadeDoElevador;
   int quantidadeDePessoas;
 
-  public Elevador(){
+  public Elevador() {
 
   }
 
-  public Elevador(int totalDeAndares, int capacidadeDoElevador, int quantidadeDePessoas){
+  public Elevador(int totalDeAndares, int capacidadeDoElevador) {
     this.totalDeAndares = totalDeAndares;
     this.capacidadeDoElevador = capacidadeDoElevador;
     this.quantidadeDePessoas = quantidadeDePessoas;
 
+  }
+
+  public void entra(int novaPessoa) {
+    if (novaPessoa <= 8) {
+      quantidadeDePessoas = quantidadeDePessoas + novaPessoa;
+    } else {
+      System.out.println("Não é permitido ultrapassar a capacidade máxima de pessoas!");
+    }
+
+  }
+
+  public void sai(int pessoaSai) {
+    if (pessoaSai > 0 & pessoaSai <= 8) {
+      quantidadeDePessoas = quantidadeDePessoas - pessoaSai;
+    }
+
+  }
+
+  public void sobe(int subirAndar) {
+    if (subirAndar < 12) {
+      andarAtual = andarAtual + subirAndar;
+    } else {
+      System.out.println("O último andar é o 12º!");
+    }
+
+  }
+
+  public void desce(int descerAndar) {
+    if (descerAndar > 0 & descerAndar <= 12) {
+      andarAtual = descerAndar;
+    } else {
+      System.out.println("NÃO HÁ PARA ONDE DESCER ALÉM DO 0!");
+    }
   }
 }
--- src/br/com/zup/exercicio3/Main.java
+++ src/br/com/zup/exercicio3/Main.java
@@ -2,6 +2,24 @@ package br.com.zup.exercicio3;
 
 public class Main {
   public static void main(String[] args) {
+    Elevador elevador = new Elevador(12, 8);
 
+    elevador.entra(8);
+    System.out.println("A quantidade atual de pessoas é: " + elevador.quantidadeDePessoas);
+    System.out.println("-------------------------");
+
+    elevador.sai(3);
+    System.out.println("Depois das saídas, a quantidade de pessoas passou a ser: " + elevador.quantidadeDePessoas);
+    System.out.println("-------------------------");
+
+    elevador.sobe(8);
+    System.out.println("Subindo ao andar escolhido: " + elevador.andarAtual);
+    System.out.println("-------------------------");
+
+    elevador.desce(5);
+    System.out.println("Descendo ao andar escolhido: " + elevador.andarAtual);
   }
+
+
 }
+
