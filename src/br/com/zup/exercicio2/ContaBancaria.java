a1b342ab4fb2601f8de17b537d9a7ec6730df2f2
Bárbara Romero <bqromero@gmail.com>
2021-09-20 15:22:44 -0300

    Criando classe Conta Bancária

--- src/br/com/zup/exercicio2/ContaBancaria.java
+++ src/br/com/zup/exercicio2/ContaBancaria.java
@@ -1,4 +1,17 @@
 package br.com.zup.exercicio2;
 
 public class ContaBancaria {
+  String nomeDoCorrentista;
+  int numeroDaConta;
+  double saldo = 0;
+
+  public ContaBancaria(){
+
+  }
+
+  public ContaBancaria(String nomeDoCorrentista, int numeroDaConta){
+    this.nomeDoCorrentista = nomeDoCorrentista;
+    this.numeroDaConta = numeroDaConta;
+  }
+
 }
