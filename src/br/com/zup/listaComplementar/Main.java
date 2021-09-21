cac630bafb74ff41c37f6b8475dbcfc173e76335
Bárbara Romero <bqromero@gmail.com>
2021-09-21 10:58:40 -0300

    Instanciando classe Bola e exibindo dados através dos métodos

--- src/br/com/zup/listaComplementar/Main.java
+++ src/br/com/zup/listaComplementar/Main.java
@@ -4,7 +4,7 @@ public class Main {
   public static void main(String[] args) {
     Bola jabulani = new Bola(10, "borracha");
 
-    jabulani.trocaCor("laranja");
+    jabulani.trocaCor("azul");
     jabulani.mostraCor();
 
   }
