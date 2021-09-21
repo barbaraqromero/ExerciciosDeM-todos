fb0cec4899d7ae3bde694379485323ae1de002ee
Bárbara Romero <bqromero@gmail.com>
2021-09-21 11:06:17 -0300

    Criando a classe Quadrado

--- src/br/com/zup/listaComplementar/Main.java
+++ src/br/com/zup/listaComplementar/Main.java
@@ -2,10 +2,18 @@ package br.com.zup.listaComplementar;
 
 public class Main {
   public static void main(String[] args) {
+    //Instanciando a classe Bola
     Bola jabulani = new Bola(10, "borracha");
 
+    //Aplicando os métodos
     jabulani.trocaCor("azul");
     jabulani.mostraCor();
+
+    //Instanciando a classe Quadrado
+    Quadrado cubo = new Quadrado(120);
+
+    cubo.mudarValorDoLado(150);
+    System.out.println(cubo.tamanhoDoLado);
 
   }
 }
--- src/br/com/zup/listaComplementar/Quadrado.java
+++ src/br/com/zup/listaComplementar/Quadrado.java
@@ -1,4 +1,30 @@
 package br.com.zup.listaComplementar;
 
 public class Quadrado {
-}
+  double tamanhoDoLado;
+
+  public Quadrado() {
+
+  }
+
+  public Quadrado(double tamanhoDoLado) {
+    this.tamanhoDoLado = tamanhoDoLado;
+
+  }
+
+  public void mudarValorDoLado(double novoValorDoLado) {
+    tamanhoDoLado = novoValorDoLado;
+  }
+
+  //public void retornarValorDoLado() {
+
+  }
+
+  //public void calcularArea() {
+
+ // }
+//}
+
+
+
+
