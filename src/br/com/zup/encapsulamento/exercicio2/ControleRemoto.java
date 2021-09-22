a5aaca4267c7232ffacabc480df93f1ad2cf91ac
Bárbara Romero <bqromero@gmail.com>
2021-09-22 18:11:33 -0300

    Criando métodos

--- src/br/com/zup/encapsulamento/exercicio2/ControleRemoto.java
+++ src/br/com/zup/encapsulamento/exercicio2/ControleRemoto.java
@@ -3,7 +3,7 @@ package br.com.zup.encapsulamento.exercicio2;
 public class ControleRemoto {
   Televisao tv;
 
-  public ControleRemoto(){
+  public ControleRemoto() {
 
   }
 
@@ -11,8 +11,32 @@ public class ControleRemoto {
     this.tv = tv;
   }
 
-  public void aumentarVolume(){
+  public void aumentarVolume() {
     int volumeAumentado = tv.getVolume() + 1;
+  }
+
+  public void diminuirVolume() {
+    int volumeDiminuido = tv.getVolume() - 1;
+  }
+
+  public void aumentarCanal() {
+    int canalAumentado = tv.getCanais() + 1;
+  }
+
+  public void diminuirCanal() {
+    int canalDiminuido = tv.getCanais() - 1;
+  }
+
+  public void canalIndicado() {
+    int novoCanal = tv.getCanais();
+  }
+
+  public int mostrarCanal() {
+    return tv.getCanais();
+  }
+
+  public int mostrarVolume() {
+    return tv.getVolume();
   }
 
 }
