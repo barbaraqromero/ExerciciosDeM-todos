34d0a60421915300524fadc6233ad2bcfa70528c
Bárbara Romero <bqromero@gmail.com>
2021-09-22 18:39:00 -0300

    Exibindo os dados através dos métodos

--- src/br/com/zup/encapsulamento/exercicio2/ControleRemoto.java
+++ src/br/com/zup/encapsulamento/exercicio2/ControleRemoto.java
@@ -13,30 +13,32 @@ public class ControleRemoto {
 
   public void aumentarVolume() {
     int volumeAumentado = tv.getVolume() + 1;
+    tv.setVolume(volumeAumentado);
   }
 
   public void diminuirVolume() {
     int volumeDiminuido = tv.getVolume() - 1;
+    tv.setVolume(volumeDiminuido);
   }
 
   public void aumentarCanal() {
     int canalAumentado = tv.getCanais() + 1;
+    tv.setCanais(canalAumentado);
   }
 
   public void diminuirCanal() {
     int canalDiminuido = tv.getCanais() - 1;
+    tv.setCanais(canalDiminuido);
   }
 
-  public void canalIndicado() {
-    int novoCanal = tv.getCanais();
+  public void canalIndicado(int novoCanal) {
+    tv.setCanais(novoCanal);
   }
 
-  public int mostrarCanal() {
-    return tv.getCanais();
+  public void mostrarDados() {
+    System.out.println("O canal é: " + tv.getCanais());
+    System.out.println("O volume é: " + tv.getVolume());
   }
 
-  public int mostrarVolume() {
-    return tv.getVolume();
-  }
 
 }
--- src/br/com/zup/encapsulamento/exercicio2/Main.java
+++ src/br/com/zup/encapsulamento/exercicio2/Main.java
@@ -2,6 +2,17 @@ package br.com.zup.encapsulamento.exercicio2;
 
 public class Main {
   public static void main(String[] args) {
+    Televisao tv = new Televisao(5, 12);
+    ControleRemoto controle = new ControleRemoto(tv);
+
+    controle.aumentarCanal();
+    controle.aumentarVolume();
+    controle.mostrarDados();
+    controle.diminuirCanal();
+    controle.diminuirVolume();
+    controle.mostrarDados();
+    controle.canalIndicado(26);
+    controle.mostrarDados();
 
   }
 }
