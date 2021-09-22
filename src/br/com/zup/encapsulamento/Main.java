ca101601d9802107001077040b44900d7c818c46
Bárbara Romero <bqromero@gmail.com>
2021-09-22 13:07:34 -0300

    Resolvendo exercício 3 de encapsulamento

---
+++ src/br/com/zup/encapsulamento/Main.java
@@ -0,0 +1,15 @@
+package br.com.zup.encapsulamento;
+
+public class Main {
+  public static void main(String[] args) {
+    Pessoa novaPessoa = new Pessoa();
+    Data dataDeNascimento = new Data(11, 9, 1989);
+
+    novaPessoa.setNome("Bárbara");
+    novaPessoa.setAltura(1.62);
+    novaPessoa.setDataDeNascimento(dataDeNascimento);
+    novaPessoa.calcularIdade(2021);
+    novaPessoa.mostrarDados();
+
+  }
+}
---
+++ src/br/com/zup/encapsulamento/Pessoa.java
@@ -0,0 +1,56 @@
+package br.com.zup.encapsulamento;
+
+public class Pessoa {
+  private String nome;
+  private Data dataDeNascimento;
+  private double altura;
+  private int idade;
+
+  public Pessoa () {
+
+  }
+
+  public Pessoa (String pessoa, Data dataDeNascimento, double altura){
+    this.nome = nome;
+    this.dataDeNascimento = dataDeNascimento;
+    this.altura = altura;
+
+  }
+
+  public String getNome(){
+    return nome;
+  }
+
+  public Data getDataDeNascimento(){
+    return dataDeNascimento;
+  }
+  public double getAltura(){
+    return getAltura();
+  }
+
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void setDataDeNascimento(Data dataDeNascimento) {
+    this.dataDeNascimento = dataDeNascimento;
+  }
+
+  public void setAltura(double altura) {
+    this.altura = altura;
+  }
+
+  public void calcularIdade(int anoAtual){
+    idade = anoAtual - dataDeNascimento.getAno();
+
+
+  }
+
+  public void mostrarDados(){
+    System.out.println("Seu nome é: " + nome);
+    System.out.println("Sua data de nascimento é: " + dataDeNascimento.getDia() + "/" + dataDeNascimento.getMes() + "/" + dataDeNascimento.getAno());
+    System.out.println("Sua idade é: " + idade);
+    System.out.println("Sua altura é: " + altura);
+  }
+}
